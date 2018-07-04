b6e4b6239b1fddb5e7ac999e760fc67c1e36f296
Fastcar48 <mkargus48@gmail.com>
2018-07-04 12:32:50 -0400

    V1.1.0
    - Added support for private repos.
    - Added messages that are return to the user.
    - Updated urls to reflect new Github username

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 //Made By Fasty48
 
-//Modules
+//Dependencies
 const express = require('express')
 const bodyParser = require("body-parser")
 const helmet = require('helmet')
 const twit = require('twit')
-
-//Config
-const port = process.env.PORT || 5000
 
 var T = new twit({
 	consumer_key: process.env.twitterConsumerKey,
@@ -21,31 +18,52 @@ var app = express()
 app.use(bodyParser.json())
 app.use(helmet())
 
-app.post('/',function(req,res){
-	if (req.query.key === process.env.key){
+function tweet(msg, response) {
+	T.post('statuses/update', { status: msg }, function (err, data) {
+		if (err) {
+			return response.status(400).send("Tweet couldn't be sent due to a error: ", err)
+		} else {
+			return response.status(200).send(`Tweet was sent out | Tweet ID: ${data.id_str}`)
+		}
+	})
+}
+
+app.post('/', function (req, res) {
+	if (req.query.key === process.env.key) {
+		//Correct key was used, so lets get the json that the site sent.
 		const commitJson = req.body
-		if (typeof commitJson.zen === 'undefined'){
+		//Github sends a "zen" to test a webhook after a user has set up a webhook.
+		if (!commitJson.zen) {
+			//There is no "zen", this is a real change so lets gather the details.
 			const repositoryName = commitJson.repository.name
 			const filesAdded = commitJson.commits[0].added.length
 			const filesModified = commitJson.commits[0].modified.length
 			const filesRemoved = commitJson.commits[0].removed.length
 			const commitURL = commitJson.commits[0].url
-			T.post('statuses/update',{status:"Changes have been made to "+repositoryName+":\n\n"+filesAdded+" files added\n"+filesModified+" files modified\n"+filesRemoved+" files removed\n\n"+commitURL}, function(err, data, response){
-				if (err){
-					console.log('There was a problem tweeting this message.', err)
-					res.sendStatus(400)
+			if (commitJson.repository.private) {
+				//The repository is private, so no need for the name or the url.
+				//But, does the user have "tweetOnPrivateRepo" set to true
+				if (process.env.tweetOnPrivateRepo) {
+					//They have "tweetOnPrivateRepo" set to true
+					tweet(`Changes have been made to a private repo:\n\n${filesAdded} files added\n${filesModified} files modified\n${filesRemoved} files removed`, res)
 				} else {
-					res.sendStatus(200)
+					res.status(200).send('Tweet was not sent out due to the "tweetOnPrivateRepo" being set to false.')
 				}
-			})
+			} else {
+				//Public repo
+				tweet(`Changes have been made to ${repositoryName}:\n\n${filesAdded} files added\n${filesModified} files modified\n${filesRemoved} files removed\n\n${commitURL}`, res)
+			}
 		} else {
-			res.sendStatus(200)
+			//"Zen" was found, so it was a test from Github to see if works.
+			//Report a 200 status code to prevent any worries.
+			res.status(200).send('Github sent a webhook test payload.')
 		}
 	} else {
-		res.sendStatus(403)
+		//Key used was not the same as the one needed, throw an error to the user informing them
+		res.status(403).send("Incorrect key is being used.\nCheck to see if key matches with the one on Heroku then click Redeliver and see if it works.")
 	}
 })
 
-app.listen(port,function(){
-	console.log('Listening on port', port)
+app.listen(process.env.PORT || 5000, function () {
+	console.log('Listening')
 })
